6803b80941f71ae5aaa0ec749cd2486db233ffa7
helenavainmaa <helena.vainmaa@fooddocs.com>
2022-11-20 19:26:12 +0200

    add svgs, do visual updates, create first chartjs chart for product

--- src/views/HumanityShop/Admin/ProductBar.vue
+++ src/views/HumanityShop/Admin/ProductBar.vue
@@ -4,15 +4,6 @@ import { Chart } from 'chart.js/auto';
 
 import { defineProps } from 'vue';
 const props = defineProps(['data']);
-// const data = [
-//     { year: 2010, count: 10 },
-//     { year: 2011, count: 20 },
-//     { year: 2012, count: 15 },
-//     { year: 2013, count: 25 },
-//     { year: 2014, count: 22 },
-//     { year: 2015, count: 30 },
-//     { year: 2016, count: 28 },
-// ];
 
 const data = reactive(props.data);
 console.log('18->', data.green);
