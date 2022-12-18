62770c326fe37a507dbb3103f86b8c59b352d20f
helenavainmaa <helena.vainmaa@fooddocs.com>
2022-12-19 00:25:35 +0200

    ...dist?

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import serveStatic from 'serve-static';
+import path from 'path';
+
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use('/', serveStatic(path.join(__dirname, '/dist')));
+
+app.listen(port);
