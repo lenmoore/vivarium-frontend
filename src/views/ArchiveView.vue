6c4c6e4f908c37751dce3fea0cc6868d01bde23c
helenavainmaa <helena.vainmaa@fooddocs.com>
2023-01-23 19:51:28 +0200

    add 23rd

--- src/views/ArchiveView.vue
+++ src/views/ArchiveView.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <h1>Arhiiv</h1>
+
         <div class="d-flex flex-column">
             <RouterLink
                 :to="{ name: 'archive.date', params: { date: '2023-01-18' } }"
@@ -17,6 +18,10 @@
             <RouterLink
                 :to="{ name: 'archive.date', params: { date: '2023-01-21' } }"
                 >21. jaanuari etendus
+            </RouterLink>
+            <RouterLink
+                :to="{ name: 'archive.date', params: { date: '2023-01-23' } }"
+                >23. jaanuari etendus
             </RouterLink>
             <a class="py-2" href="/kava">Vaata kavalehte</a>
         </div>
